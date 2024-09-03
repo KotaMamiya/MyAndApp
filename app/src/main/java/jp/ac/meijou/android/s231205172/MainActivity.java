e1c90a2ec26fbf031aa3edbc9625c317f5facddf
Kota Mamiya <147830569+KotaMamiya@users.noreply.github.com>
2024-09-03 15:27:19 +0900

    データストア

--- app/src/main/java/jp/ac/meijou/android/s231205172/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s231205172/MainActivity.java
@@ -1,11 +1,16 @@
 package jp.ac.meijou.android.s231205172;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,21 +20,51 @@ import jp.ac.meijou.android.s231205172.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private  PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        prefDataStore = PrefDataStore.getInstance(this);
+        prefDataStore.getString("name").ifPresent(name -> binding.text.setText(name));
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        TextView text = findViewById(R.id.text);
-        text.setText(R.string.name);
-        binding.text.setText(R.string.name);
+
+        binding.button.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+            var icon = ContextCompat.getDrawable(this, R.drawable.ic_andyel);
+            binding.imageView.setImageDrawable(icon);
+        });
+
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
+        });
+
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
+        });
     }
 }
